0ece0c8e55a9aea3a5b982137a18f41635272713
soficasares <102155698+soficasares@users.noreply.github.com>
2022-04-26 09:19:46 -0300

    button finished

---
+++ Assets/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+
+    public void PressButton() {
+        int counter = 0;
+
+        while (counter < 5)
+        {
+            Debug.Log ("Hola mundo!");
+            counter++;
+        }
+
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
